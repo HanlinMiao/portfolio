148e88516339400c455376875db3581a079cd09d
HanlinMiao <hanlin980505@gmail.com>
2025-01-09 13:20:11 -0500

    Build Contact Component

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -5,7 +5,7 @@ import { assets, workData } from '@/assets/assets'
 const Work = () => {
   return (
     <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
-       <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
+        <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
         <h2 className="text-center text-5xl font-Ovo">My Latest Work</h2>
         <p className="text-center max-w-2xl mx-auto mt-5 mt-5 mb-12 font-Ovo">
             Welcome to my web development portfolio. Explore a collection of projects showcasing my
